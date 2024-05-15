123d454ad67cff3f650cd957ec7575e6226a1aca
Fernando Rodrigues <fernando111oficina@gmail.com>
2024-05-15 18:09:08 +0200

    hookform validation

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,12 +1,39 @@
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styles";
+import { useForm } from "react-hook-form";
+import * as z from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const searchFormSchema = z.object({
+  query: z.string().min(3).max(100)
+})
+
+type searchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+  const { 
+    register, 
+    handleSubmit,
+    formState: { isSubmitting }
+  } = useForm<searchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  async function handleSearchTransactions(data: searchFormInputs) {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    console.log(data)
+  }
+  
   return (
     <div>
-      <SearchFormContainer>
-        <input type="text"  placeholder="Search for transations"/>
-        <button type="submit">
+      <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+        <input 
+          type="text"  
+          placeholder="Search for transations"
+          {...register('query')}
+        />
+        <button type="submit" disabled={isSubmitting}>
           <MagnifyingGlass size={20}/>
           Search
         </button>
